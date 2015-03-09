1f842203f0b1a6f1ceeb1248bcdd64a16f6dcfe4
Gerardo Camilo <gerardo.camilo@gmail.com>
2015-03-08 21:24:19 -0400

    Creating a map with latitude, longitude, region and setting a "place" using a MKPointAnnotation.

--- iOS8SwiftMApKitTutorial/ViewController.swift
+++ iOS8SwiftMApKitTutorial/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let location = CLLocationCoordinate2D(latitude: 18.473023, longitude:-69.887120)
+
+        let span = MKCoordinateSpanMake(0.05, 0.05)
+        let region = MKCoordinateRegionMake(location, span)
+        mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.setCoordinate(location)
+        annotation.title = "The place you're looking for"
+        annotation.subtitle = "Zona Colonial, Santo Domingo"
+        mapView.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
